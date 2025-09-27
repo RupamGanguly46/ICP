ce2b40e9c6cef27774043cb66236694a0f1ca20a
RupamGanguly46 <rupam.ganguly46@gmail.com>
2025-09-27 13:43:07 +0530

    Third Problem solved

---
+++ LC_628_Maximum_product_of_three_numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class LC_628_Maximum_product_of_three_numbers {
+    public static int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        int n = nums.length;
+        return Math.max(nums[n-1]*nums[n-2]*nums[n-3], nums[0]*nums[1]*nums[n-1]);
+    }
+    public static void main(String[] args) {
+        System.out.println(maximumProduct(new int[] {-100, -90, -30, -5}));
+        System.out.println(maximumProduct(new int[] {-100, -90, 2, 5}));
+        System.out.println(maximumProduct(new int[] {-100, -90, 92, 95, 100}));
+        // -100*-90*100 vs 100*95*92
+    }
+}
