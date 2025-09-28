2342befea12ebd3ece8198248e92bf87c026dd65
RupamGanguly46 <rupam.ganguly46@gmail.com>
2025-09-28 10:04:00 +0530

    Update

---
+++ LC_215_Kth_largest_element_in_an_array.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class LC_215_Kth_largest_element_in_an_array{
+    public static int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
