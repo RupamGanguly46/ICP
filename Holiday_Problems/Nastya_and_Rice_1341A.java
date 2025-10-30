81f9361420d04c1112ecae558f8fbe2d00ccccd7
RupamGanguly46 <rupam.ganguly46@gmail.com>
2025-10-30 22:29:48 +0530

    Holiday problems

--- Holiday_Problems/Nastya_and_Rice_1341A.java
+++ Holiday_Problems/Nastya_and_Rice_1341A.java
@@ -15,4 +15,24 @@ public class Nastya_and_Rice_1341A {
             }
         }
     }
+
+    public class Main2{
+        static boolean f(int n,int a,int b,int c,int d){
+            int min=n*(a-b),max=n*(a+b);
+            int low=c-d,high=c+d;
+            if(max<low||min>high)return false;
+            return true;
+        }
+        static void solve(Scanner s,int t){
+            if(t==0)return;
+            int n=s.nextInt(),a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt();
+            System.out.println(f(n,a,b,c,d)?"Yes":"No");
+            solve(s,t-1);
+        }
+        public static void main(String[]args){
+            Scanner s=new Scanner(System.in);
+            int t=s.nextInt();
+            solve(s,t);
+        }
+    }
 }
